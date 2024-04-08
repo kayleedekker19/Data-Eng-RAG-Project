f569f5eb5cb492fe6bebe858f380b31fd9e5a4d2
kayleedekker19 <kayleedekker19@gmail.com>
2024-04-08 12:27:55 +0200

    Update file paths

--- run_pipeline.sh
+++ run_pipeline.sh
@@ -7,11 +7,11 @@ echo "Starting the KFP pipeline process..."
 
 # Setup input and output variables
 INPUT_CSV="restaurants_names_entry.csv" # Adjust this to your actual input file name, if needed
-OUTPUT_PATH="processed_output" # Adjust this to your desired output path in the bucket
+OUTPUT_PATH="processed_output_with_docker" # Adjust this to your desired output path in the bucket
 
 # Running the Python script with input and output arguments
 echo "Running the pipeline with input: $INPUT_CSV and output: $OUTPUT_PATH"
-python3 /Users/kayleedekker/PycharmProjects/Data-Eng-RAG-Project/vertex_ai/kfp_pipeline.py --input_csv $INPUT_CSV --output_path $OUTPUT_PATH
+python3 vertex_ai/kfp_pipeline.py --input_csv $INPUT_CSV --output_path $OUTPUT_PATH
 
 if [ $? -eq 0 ]; then
     echo "Pipeline completed successfully and data uploaded to: $OUTPUT_PATH"
