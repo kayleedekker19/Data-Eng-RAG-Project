52be187f2eac7b36d17fcfd692e45d1f0fd7c204
kayleedekker19 <kayleedekker19@gmail.com>
2024-04-08 14:06:47 +0200

    update host

--- 8_rag_api_end_point_flask/rag_memory_end_point.py
+++ 8_rag_api_end_point_flask/rag_memory_end_point.py
@@ -78,4 +78,4 @@ def ask_question(input_question):
     chat_history.append(AIMessage(content=answer))
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
